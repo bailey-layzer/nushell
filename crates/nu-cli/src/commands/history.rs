4036335401210b28d269bc4c440354019437a359
Bailey Layzer <bailey.layzer@gmail.com>
2020-07-31 10:19:50 -0700

    initial

--- crates/nu-cli/src/commands/history.rs
+++ crates/nu-cli/src/commands/history.rs
@@ -3,10 +3,16 @@ use crate::commands::WholeStreamCommand;
 use crate::prelude::*;
 use nu_errors::ShellError;
 use nu_protocol::{ReturnSuccess, Signature, UntaggedValue};
+use nu_source::Tagged;
 use std::fs::File;
 use std::io::{BufRead, BufReader};
 
 pub struct History;
+
+#[derive(Deserialize)]
+pub struct HistoryArgs {
+    structured: Tagged<bool>,
+}
 
 #[async_trait]
 impl WholeStreamCommand for History {
@@ -15,7 +21,11 @@ impl WholeStreamCommand for History {
     }
 
     fn signature(&self) -> Signature {
-        Signature::build("history")
+        Signature::build("history").switch(
+            "structured",
+            "output retrieved history as row",
+            Some('s'), // TODO example for this
+        )
     }
 
     fn usage(&self) -> &str {
@@ -29,10 +39,19 @@ impl WholeStreamCommand for History {
     ) -> Result<OutputStream, ShellError> {
         history(args, registry)
     }
+
+    fn examples(&self) -> Vec<Example> {
+        vec![Example {
+            description: "TODO",
+            example: r#"history -s | last 1 | TODO"#,
+            result: None, // TODO probably can't test here
+        }]
+    }
 }
 
 fn history(args: CommandArgs, _registry: &CommandRegistry) -> Result<OutputStream, ShellError> {
     let tag = args.call_info.name_tag;
+    let (HistoryArgs { structured }, _) = args.process(&registry).await?;
     let history_path = HistoryFile::path();
     let file = File::open(history_path);
     if let Ok(file) = file {
@@ -44,7 +63,11 @@ fn history(args: CommandArgs, _registry: &CommandRegistry) -> Result<OutputStrea
             Err(_) => None,
         });
 
-        Ok(futures::stream::iter(output).to_output_stream())
+        if structured.item {
+            Err(ShellError::unimplemented("structured history"))
+        } else {
+            Ok(futures::stream::iter(output).to_output_stream())
+        }
     } else {
         Err(ShellError::labeled_error(
             "Could not open history",
